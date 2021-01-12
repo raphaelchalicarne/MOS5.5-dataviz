72e35dbc48500653889f592c1a5afec1a952409e
raphaelchalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2021-01-12 11:06:45 +0100

    Get audio features & track name & track artist of the top200 of France in 2020

--- read_csv_spotify.py
+++ read_csv_spotify.py
@@ -18,26 +18,49 @@ import os
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+
+def get_artists(track_data):
+    artist_list = [artist_dict['name']
+                   for artist_dict in track_data['artists']]
+    return ";".join(artist_list)
+
+
 country_list = ['france', 'bresil', 'allemagne', 'royaume-uni', 'espagne', 'italie', 'chili', 'colombie',
                 'suisse', 'bolivie', 'autriche', 'belgique', 'equateur', 'danemark', 'republique_tcheque', 'paraguay']
 
-# country = 'france'
-for country in country_list:
-    csv_array = np.loadtxt('top_' + country + '_200_spotify.csv', delimiter=",", dtype=object)
-    spotify_dict = {}
-    for position_i in range(1, 201):
-        for date_j in range(1, 367):
-            track_id = csv_array[position_i, date_j]
-            if (spotify_dict.get(track_id) == None):
-                spotify_dict[track_id] = sp.audio_features(track_id)
+country = 'france'
+# for country in country_list:
+csv_array = np.loadtxt(
+    'top_' + country + '_200_spotify.csv', delimiter=",", dtype=object)
+spotify_dict = {}
+for position_i in range(1, 201):
+    for date_j in range(1, 367):
+        track_id = csv_array[position_i, date_j]
+        if (spotify_dict.get(track_id) == None):
+            track_af = sp.audio_features(track_id)[0]
+            if track_af != None:
+                spotify_dict[track_id] = track_af
+                
+                track_data = sp.track(track_id)
+                track_name = track_data['name']
+                track_artists = get_artists(track_data)
     
+                spotify_dict[track_id]['name'] = track_name
+                spotify_dict[track_id]['artists'] = track_artists
+
 with open('top_200_af_spotify.csv', mode='w', newline='') as csv_spotify_af:
-    fieldnames = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
-    writer = csv.DictWriter(csv_spotify_af, fieldnames=fieldnames)
-    
+    fieldnames = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness',
+                  'valence', 'tempo', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature', 'name', 'artists']
+    writer = csv.DictWriter(csv_spotify_af, fieldnames=fieldnames,
+                            delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
     writer.writeheader()
     for track_af in spotify_dict.values():
-        if track_af[0] != None:
-            writer.writerow(track_af[0])
+        if track_af != None:
+            writer.writerow(track_af)
 
 
+# %%
+bo_track_id = '717QGTmZdkemDaCo1vQHG0'
+bo_track_data = sp.track(bo_track_id)
+get_artists(bo_track_data)
