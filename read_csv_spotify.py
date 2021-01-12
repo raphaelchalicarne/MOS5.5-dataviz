19c763e3b9daeb19e877b05d38175ee256d83063
raphaelchalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2021-01-12 10:28:32 +0100

    Get audio features from all top200 tracks of 2020

--- read_csv_spotify.py
+++ read_csv_spotify.py
@@ -21,12 +21,23 @@ sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 country_list = ['france', 'bresil', 'allemagne', 'royaume-uni', 'espagne', 'italie', 'chili', 'colombie',
                 'suisse', 'bolivie', 'autriche', 'belgique', 'equateur', 'danemark', 'republique_tcheque', 'paraguay']
 
-country = 'france'
+# country = 'france'
+for country in country_list:
+    csv_array = np.loadtxt('top_' + country + '_200_spotify.csv', delimiter=",", dtype=object)
+    spotify_dict = {}
+    for position_i in range(1, 201):
+        for date_j in range(1, 367):
+            track_id = csv_array[position_i, date_j]
+            if (spotify_dict.get(track_id) == None):
+                spotify_dict[track_id] = sp.audio_features(track_id)
+    
+with open('top_200_af_spotify.csv', mode='w', newline='') as csv_spotify_af:
+    fieldnames = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+    writer = csv.DictWriter(csv_spotify_af, fieldnames=fieldnames)
+    
+    writer.writeheader()
+    for track_af in spotify_dict.values():
+        if track_af[0] != None:
+            writer.writerow(track_af[0])
 
-csv_array = np.loadtxt('top_' + country + '_200_spotify.csv', delimiter=",", dtype=object)
-spotify_france_dict = {}
-for position_i in range(1, 201):
-    for date_j in range(1, 367):
-        track_id = csv_array[position_i, date_j]
-        if (spotify_france_dict.get(track_id) == None):
-            spotify_france_dict[track_id] = sp.audio_features(track_id)
+
